6316ea50d24a40e053b139077880d53edfc18cb3
bkapp15 <bak792@utexas.edu>
2017-12-29 02:28:08 -0600

    Fix #1 Resolved final mysql connection error in ajax

--- user_emailCheck.php
+++ user_emailCheck.php
@@ -1,48 +1,30 @@
 <?php
-
+	
+	require_once('custom_mysqli.php');
+	
   if (isset($_GET["userName"])){
     $username = $_GET["userName"];
     $table = "Users";
-    $mysqli = connect();
-    $result = mysqli_query($mysqli, "SELECT * from $table WHERE username='$username'");
-    $row = $result->fetch_row();
+    $dbConnection = db_connect();
+    $result = mysqli_query($dbConnection, "SELECT * from $table WHERE username='$username'");
+    $row = mysqli_fetch_assoc($result);
     if ($row){
       echo "The username you entered already exists.";
-      $result->free();
-      return; 
     }
-    $result->free();
+    db_disconnect($dbConnection, $result);
+    return; 
   }
 
   elseif (isset($_GET["email"])){
     $email = $_GET["email"];
     $table = "Users";
-    $mysqli = connect();
-    $result = mysqli_query($mysqli, "SELECT * from $table WHERE email='$email'");
-    $row = $result->fetch_row();
+    $dbConnection = db_connect();
+    $result = mysqli_query($dbConnection, "SELECT * from $table WHERE email='$email'");
+    $row = mysqli_fetch_assoc($result);
     if ($row){
       echo "The email you entered is already in use.";
-      $result->free();
-      return;
     }
-    $result->free();
-  }
-
-
-  function connect(){
-    $host = "fall-2016.cs.utexas.edu";
-    $user = "tylrnoe";
-    $pwd = openssl_decrypt("Wn07mUreOuTUaGy2cBdUSg==", 'aes-128-cbc', 'acapella');
-    $dbs = "cs329e_tylrnoe";
-    $port = "3306";
-
-    $connection = mysqli_connect($host, $user, $pwd, $dbs, $port);
-
-    if (empty($connection)){
-      die("mysqli_connect failed" . mysqli_connect_error());
-    }
-
-    return $connection;
-    //print "Connected to " . mysqli_get_host_info($connect) . "<br/><br/> \n";
+    db_disconnect($dbConnection, $result);
+    return;
   }
 ?>
